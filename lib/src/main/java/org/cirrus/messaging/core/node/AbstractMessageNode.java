76ba53262ec409b19594723e2317dd7f024115d4
rtatton <rdt17@protonmail.com>
2021-09-17 09:38:52 -0400

    Rename MessageNode to AbstractMessageNode
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- lib/src/main/java/org/cirrus/messaging/core/node/AbstractMessageNode.java
+++ lib/src/main/java/org/cirrus/messaging/core/node/AbstractMessageNode.java
@@ -5,11 +5,11 @@ import org.cirrus.messaging.core.message.MessageFactory;
 import org.cirrus.messaging.core.pubsub.Publisher;
 import org.cirrus.messaging.core.pubsub.Subscriber;
 
-public abstract class MessageNode extends AbstractNode<Message> {
+public abstract class AbstractMessageNode extends AbstractNode<Message> {
 
   private final MessageFactory messageFactory;
 
-  protected MessageNode(Publisher<Message> publisher, Subscriber subscriber,
+  protected AbstractMessageNode(Publisher<Message> publisher, Subscriber subscriber,
       MessageFactory messageFactory) {
     super(publisher, subscriber);
     this.messageFactory = messageFactory;
