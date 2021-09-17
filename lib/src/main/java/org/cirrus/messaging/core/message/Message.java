6172e97031979a8721ab88608faa1080dd74f5f1
rtatton <rdt17@protonmail.com>
2021-09-17 14:11:14 -0400

    Fix JavadocMethod pattern violation
    Codacy code style
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- lib/src/main/java/org/cirrus/messaging/core/message/Message.java
+++ lib/src/main/java/org/cirrus/messaging/core/message/Message.java
@@ -5,6 +5,8 @@ public interface Message {
   /**
    * The sender is the endpoint from which the message originated. The sender may differ from the
    * return address.
+   *
+   * @return The sender associated with the message.
    */
   public abstract String getSender();
 
@@ -12,6 +14,8 @@ public interface Message {
    * The return address is an endpoint to which messages can be sent in response to this message.
    * This allows for request-reply messaging where the message sender enables the receiver to send a
    * response.
+   *
+   * @return The return address associated with the message.
    */
   public abstract String getReturnAddress();
 
@@ -21,12 +25,16 @@ public interface Message {
    * messages to the subscriber. The subscription identifier is typically created when a subscriber
    * initially subscribes to the publisher. This identifier can then be used to cancel the
    * subscription.
+   *
+   * @return The subscription associated with the message.
    */
   public abstract String getSubscription();
 
   /**
    * The body is the content or data contained in the message. The syntax and semantics of the body
    * will vary based on the implementation. All non-{@link String} bodies should be serializable.
+   *
+   * @return The body associated with the message.
    */
   public abstract String getBody();
 
