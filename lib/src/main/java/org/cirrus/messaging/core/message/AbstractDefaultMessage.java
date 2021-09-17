6e8a654ab5b9c4235f1ffcb846907f340d1ae852
rtatton <rdt17@protonmail.com>
2021-09-17 12:09:57 -0400

    Address Codacy code style checks
    - Add class Javadoc
    - Enforce non-null, non-empty on sender, returnAddress, and body
    - Make @Value.Check methods final
    - Modify ImmutableStyle class naming
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

---
+++ lib/src/main/java/org/cirrus/messaging/core/message/AbstractDefaultMessage.java
@@ -0,0 +1,48 @@
+package org.cirrus.messaging.core.message;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import org.immutables.value.Value;
+
+/**
+ * A {@link Message} that requires the sender, return address, and body to be non-null and
+ * non-empty.
+ */
+@Value.Immutable
+public abstract class AbstractDefaultMessage implements Message {
+
+  private static final String NO_SUBSCRIPTION = null;
+  private static final String INVALID_BODY = "'body' must not be empty or null";
+  private static final String INVALID_SENDER = "'sender' must not be empty or null";
+  private static final String INVALID_ADDRESS = "'returnAddress' must not be empty or null";
+
+  @Override
+  public abstract String getSender();
+
+  @Override
+  public abstract String getReturnAddress();
+
+  @Override
+  @Value.Default
+  public String getSubscription() {
+    return NO_SUBSCRIPTION;
+  }
+
+  @Override
+  public abstract String getBody();
+
+  @Value.Check
+  protected final void checkBody() {
+    Preconditions.checkState(Strings.isNullOrEmpty(getBody()), INVALID_BODY);
+  }
+
+  @Value.Check
+  protected final void checkSender() {
+    Preconditions.checkState(Strings.isNullOrEmpty(getSender()), INVALID_SENDER);
+  }
+
+  @Value.Check
+  protected final void checkReturnAddress() {
+    Preconditions.checkState(Strings.isNullOrEmpty(getReturnAddress()), INVALID_ADDRESS);
+  }
+}
